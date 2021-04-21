b5cfdb25b5f3c069b36da98337fa4b665e227130
Guliaiev Andrey <andrguliaiev@gmail.com>
2021-04-21 15:28:52 +0300

    Manager2

---
+++ src/main/java/ru/netology/domain/Product.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Product {
+    private int id;
+    private String name;
+    private int price;
+}
